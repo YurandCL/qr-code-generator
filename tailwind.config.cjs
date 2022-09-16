6f3cc830e048046ee6c217ff170017ad260ad2d0
Yurand <yurand.coloma@tecsup.edu.pe>
2022-09-16 17:50:28 -0500

    config project

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        button: { cursor: 'default' }
+      })
+    })
+  ]
+}
